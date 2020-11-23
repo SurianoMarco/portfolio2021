31211af794f5182af91d45b79df7b6fcb8b13ff2
Marco Suriano <marcosuriano@outlook.com>
2020-11-23 22:41:47 +0100

    Add

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ const html = document.documentElement;
 const canvas = document.getElementById("hero-lightpass");
 const context = canvas.getContext("2d");
 
-const frameCount = 147;
+const frameCount = 160;
 const currentFrame = index => (
-  `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${index.toString().padStart(4, '0')}.jpg`
+  `assets/img/parallax/${index.toString().padStart(4, '0')}.png`
+  
 )
 
 const preloadImages = () => {
@@ -29,8 +30,8 @@ window.addEventListener("scroll", (event) => {
 
 const img = new Image()
 img.src = currentFrame(1);
-canvas.width=1158;
-canvas.height=770;
+canvas.width=3840;
+canvas.height=2160;
 img.onload=function(){
   context.drawImage(img, 0, 0);
 }
