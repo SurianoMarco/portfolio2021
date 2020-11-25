799a07c521afccc1c63ec94046547eeae95bb10e
Marco Suriano <marcosuriano@outlook.com>
2020-11-25 12:10:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const html = document.documentElement;
 const canvas = document.getElementById("hero-lightpass");
 const context = canvas.getContext("2d");
 
-const frameCount = 114;
+const frameCount = 160;
 const currentFrame = index => (
   `assets/img/parallax/${index.toString().padStart(4, '0')}.jpg`
 )
@@ -11,26 +11,13 @@ const preloadImages = () => {
   for (let i = 1; i < frameCount; i++) {
     const img = new Image();
     img.src = currentFrame(i);
-    }
-  
+  }
 };
-
-window.addEventListener("scroll", (event) => {
-  let scroll1 = this.scrollY;
-  if(scroll1>=200){
-    canvas.style.opacity = "0"
-  }
-  else{
-    canvas.style.opacity = "1"
-  }
-  console.log(scroll)
-});
-
 
 const img = new Image()
 img.src = currentFrame(1);
 canvas.width=1920;
-canvas.height=381;
+canvas.height=770;
 img.onload=function(){
   context.drawImage(img, 0, 0);
 }
@@ -43,7 +30,7 @@ const updateImage = index => {
 window.addEventListener('scroll', () => {  
   const scrollTop = html.scrollTop;
   const maxScrollTop = html.scrollHeight - window.innerHeight;
-  const scrollFraction = scrollTop / 200;
+  const scrollFraction = scrollTop / maxScrollTop;
   const frameIndex = Math.min(
     frameCount - 1,
     Math.ceil(scrollFraction * frameCount)
@@ -54,5 +41,5 @@ window.addEventListener('scroll', () => {
 
 
 
-
 preloadImages()
+
